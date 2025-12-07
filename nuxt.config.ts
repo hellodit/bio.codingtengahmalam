edb20c6b8f72a6875af4128ac78ab4d6bf9f44a5
hellodit <asditap@gmail.com>
2025-12-07 13:16:21 +0700

    Add @nuxt/content module and update dependencies in package.json and nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   modules: [
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxt/content'
   ],
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true }
