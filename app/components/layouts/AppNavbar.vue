8cf6e1e61981177264530221ccad395cca05abbe
hellodit <asditap@gmail.com>
2025-12-20 12:01:08 +0700

    Update AppNavbar.vue to enhance layout by centering the navigation content within a maximum width, improving overall visual presentation and consistency across the application.

--- app/components/layouts/AppNavbar.vue
+++ app/components/layouts/AppNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="border-b border-border py-2 px-4 md:sticky md:top-0 md:z-10 bg-white dark:bg-gray-800">
     <UContainer>
-      <div class="flex items-center justify-between h-10">
+      <div class="max-w-xl mx-auto flex items-center justify-between h-10">
         <div class="flex items-center gap-4">
           <UButton
             to="/"
